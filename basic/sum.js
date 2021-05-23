43356a98ec08febcceaef92212364d1dc0b150fe
KAO-TZU-WEI <sunvicky11@gmail.com>
2021-05-23 13:32:33 +0800

    change the for loop

--- basic/sum.js
+++ basic/sum.js
@@ -1,7 +1,7 @@
 console.log("hello");
 function sum(item){
     let total=0
-    for(i=1;i<(item+1);i++){
+    for(let i=1;i<(item+1);i++){
         total+=i
     }
     return total
@@ -10,3 +10,4 @@ console.log(sum(1)); // 1
 console.log(sum(2)); // 3
 console.log(sum(10)); // 55
 console.log(sum(100000)); 
+//千萬要注意for迴圈內是區域變數let
