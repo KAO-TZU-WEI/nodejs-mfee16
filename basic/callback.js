b4ec5af0b4320ae3c9419cff3309dcbb6052ed40
KAO-TZU-WEI <sunvicky11@gmail.com>
2021-05-29 17:58:35 +0800

    add new function

--- basic/callback.js
+++ basic/callback.js
@@ -1,27 +1,34 @@
 let doWork = function (job, timer, cb) {
     setTimeout(() => {
-      let dt = new Date();
-      cb(null, `完成工作: ${job} at ${dt.toISOString()}`);
+        let dt = new Date();
+        cb(null, `完成工作: ${job} at ${dt.toISOString()}`);
     }, timer);
-  };
-  
-  let dt = new Date();
-  console.log(`開始工作 at ${dt.toISOString()}`);
-  doWork("刷牙", 2000, function (err, result) {
+};
+
+let dt = new Date();
+console.log(`開始工作 at ${dt.toISOString()}`);
+doWork("刷牙", 2000, function (err, result) {
     if (err) {
-      console.error(err);
-      return;
+        console.error(err);
+        return;
     }
+    console.log(result);
     doWork("吃早餐", 3000, function (err, result) {
         if (err) {
-          console.error(err);
-          return;
+            console.error(err);
+            return;
         }
         console.log(result);
-      });
-      console.log(result);
-  });
-  
+    });
+    doWork("寫功課", 6000, function (err, result) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log(result);
+    });
+});
+
 //   doWork("吃早餐", 3000, function (err, result) {
 //     if (err) {
 //       console.error(err);
