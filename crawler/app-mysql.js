76852df493bd86b765f03e7e63d47c32be9a30e9
KAO-TZU-WEI <sunvicky11@gmail.com>
2021-06-13 00:29:18 +0800

    updated await and promise two style for this file

--- crawler/app-mysql.js
+++ crawler/app-mysql.js
@@ -12,6 +12,8 @@ const Promise = require("bluebird");
 const fsBlue = Promise.promisifyAll(fs);
 //使用mysql套件
 const mysql = require("mysql");
+const Bluebird = require("bluebird");
+const { resolve } = require("bluebird");
 //使用dotenv-密碼隱藏
 require("dotenv").config();
 //連線資料庫
@@ -21,68 +23,128 @@ var connection = mysql.createConnection({
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
 });
+connection = Bluebird.promisifyAll(connection);
 // mysql本身是沒有Promise的，所以用bluebird來將連線Promise化
-//connection = Promise.promisifyAll(connection);
-// connection.connectAsync();
-//確認是否連線
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: ");
-    return;
-  }
-  console.log("connected");
-});
-//讀取成功
 
-//建立非同步函式連結資料庫後，讀取連結
-//(async function () {
-//建立連結，(blue後需加上Async)
+//寫法一：await
+// (async function () {
+//   try {
+//     await connection.connectAsync();
+//     let stockNum = await fsBlue.readFileAsync("stock.txt", "utf8");
+//     let result = await connection.queryAsync(
+//       `SELECT stock_id FROM stock WHERE stock_id = ${stockNum}`
+//     );
+//     if (result.length >= 1) {
+//       console.log("這一支股票已經存在");
+//     } else {
+//       let search = await axios.get(
+//         `https://www.twse.com.tw/zh/api/codeQuery?query=${stockNum}`
+//       );
+//       let results = search.data.suggestions;
+//       let result = results.shift().split("\t");
+//       if (result.length > 1) {
+//         let final = await connection.queryAsync(
+//           `INSERT INTO stock(stock_id, stock_name) VALUES ('${result[0]}','${result[1]}')`
+//         );
+//       } else {
+//         console.log("您輸入的代號錯誤");
+//       }
+//     }
+//     console.log(`查詢股票代號： ${stockNum}成交資料`);
+//     let detail = await axios.get(
+//       `https://www.twse.com.tw/exchangeReport/STOCK_DAY`,
+//       {
+//         params: {
+//           response: "json",
+//           date: moment().format("YYYYMMDD"),
+//           stockNo: stockNum,
+//         },
+//       }
+//     );
+//     //console.log(moment().format("YYYYMMDD"));
+//     if ((detail.stat = "OK")) {
+//       //let stockDetail = detail.data.data;
+//       //console.log(stockDetail.length);
+//       let stockDetail = detail.data.data.map((item) => {
+//         item = item.map((value) => {
+//           return value.replace(/,/g, "");
+//         });
+//         //原先"110/06/09","312,756,840","7,655,573,035","24.85",25.25","23.80","23.85","-1.00","79,012",];
+//         //修正後'2021-06-01', '666680370','17473424042', '26.30','26.80','25.70','25.70','-0.15','151625']
+//         item[0] = parseInt(item[0].replace(/\//g, ""), 10) + 19110000;
+//         item[0] = moment(item[0], "YYYYMMDD").format("YYYY-MM-DD");
+//         item.unshift(stockNum);
+//         return item;
+//       });
+//       //console.log(stockDetail);
+//       let insertDetail = await connection.queryAsync(
+//         "INSERT IGNORE INTO stock_price (stock_id, date, volume, amount, open_price, high_price, low_price, close_price, delta_price, transactions) VALUES ?",
+//         [stockDetail]
+//       );
+//       console.log(insertDetail); //OkPacket是成功時會出現的
+//     }
+//   } catch (err) {
+//     console.error(err);
+//   } finally {
+//     connection.end();
+//   }
+// })();
 
+//寫法二：promise-blue後需加上Async
+var insertNum = "";
 fsBlue
   .readFileAsync("stock.txt", "utf8")
-  .then((value) => {
-    return axios.get("https://www.twse.com.tw/zh/api/codeQuery", {
-      params: {
-        query: value,
-      },
-    });
-  })
-  .then(function (response) {
-    connection.query(
-      `SELECT stock_id FROM stock WHERE stock_id ='${response.data.query}'`,
-      function (error, results, fields) {
-        //console.log("結果是", results.length, fields);
-        if (results.length == 0) {
-          console.log("結果是", results.length);
-          judge();
-        } else if (error) throw error;
-      }
-    );
-    //judge();
-    function judge() {
-      if (response.status == "200") {
-        //確定可以抓到狀態是200時的資料，接著取出response.data.suggestion的內容（物件）
-        //股票代號所搜尋出來相對應的值可能不止一個，但在本次練習只需要抓取一個，並拆分成股票id和名稱
-        let data = response.data.suggestions.shift();
-        let newdata = data.split("\t");
-        console.log("這是抓出來並要keyin到資料庫的", newdata);
-        if (data.length > 1) {
-          connection.query(
-            `INSERT INTO stock(stock_id, stock_name) VALUES ('${newdata[0]}','${newdata[1]}')`
-          );
-          //若是資料已經有資料，則會出現code: 'ER_DUP_ENTRY'和errno: 1062和sqlState: '23000'的錯誤代碼
+  .then((stockNum) => {
+    console.log("目前查詢的是", stockNum);
+    insertNum = stockNum;
+    return (numResult = new Promise((resolve, reject) => {
+      connection.query(
+        `SELECT stock_id FROM stock WHERE stock_id = ${stockNum}`,
+        function (err, results) {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(results);
+          }
         }
-      }
+      );
+    }));
+  })
+  .then((resultNum) => {
+    //console.log("我在這", resultNum.length);
+    if (resultNum.length >= 1) {
+      //throw console.log("已經存在這支股票");
+      return Promise.resolve(false);
+    } else {
+      return axios.get(
+        `https://www.twse.com.tw/zh/api/codeQuery?query=${insertNum} `
+      );
+    }
+  })
+  .then((results) => {
+    if (results != false) {
+      let result = results.data.suggestions.shift().split("\t");
+      console.log("那我呢:", result);
+      return (final = new Promise((resolve, reject) => {
+        connection.queryAsync(
+          `INSERT INTO stock(stock_id, stock_name) VALUES ('${result[0]}','${result[1]}')`,
+          function (err, results) {
+            if (err) {
+              reject(err);
+            } else {
+              resolve(results);
+            }
+          }
+        );
+      }));
+    } else {
+      console.log("存入失敗");
     }
   })
   .catch((reason) => {
     console.log(`因為${reason}所以錯誤`);
   })
-
   .finally(() => {
-    console.log("跑到我嘍");
-    //connection.end();
+    console.log("要關門嘍");
+    connection.end();
   });
-// connection.end();
-// console.log("關閉資料庫連結嘍");
-//})();
