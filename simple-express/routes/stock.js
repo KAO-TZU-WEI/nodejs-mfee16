101c6dd61af61ba0ad047dc612fb6b03b0bacf89
KAO-TZU-WEI <sunvicky11@gmail.com>
2021-06-19 17:46:58 +0800

    add pagination function

--- simple-express/routes/stock.js
+++ simple-express/routes/stock.js
@@ -9,11 +9,38 @@ router.get("/", async function (req, res) {
 });
 router.get("/:stockCode", async (req, res) => {
   // req.params.stockCode 可以取得路由變數
-  let resultList = await data.connection.queryAsync(
-    `SELECT * FROM stock_price WHERE stock_id = ?`,
+  let stock = await data.connection.queryAsync(
+    `SELECT * FROM stock WHERE stock_id=?`,
     req.params.stockCode
   );
-  res.render("stock/detail", { resultList });
+  if (stock.length === 0) {
+    throw new Error("不存在此代碼");
+  } else {
+    //一頁有幾筆？
+    const perPage = 5;
+    //現在在第幾頁？
+    const currentPage = req.query.page || 1;
+    const offset = (currentPage - 1) * perPage;
+    //抓取全部資料
+    let resultList = await data.connection.queryAsync(
+      `SELECT * FROM stock_price WHERE stock_id = ? ORDER BY date LIMIT ? OFFSET ?`,
+      [req.params.stockCode, perPage, offset]
+    );
+    //總共有幾筆
+    let totalPage = await data.connection.queryAsync(
+      `SELECT COUNT(*) as total FROM stock_price WHERE stock_id=?`,
+      req.params.stockCode
+    );
+    //總頁數
+    let lastPage = Math.ceil(totalPage[0].total / perPage);
+    res.render("stock/detail", {
+      resultList,
+      stock,
+      pagination: { currentPage, lastPage },
+    });
+    //console.log(currentPage, totalPage[0], lastPage);
+    //next();
+  }
 });
 
 module.exports = router;
--- simple-express/utils/db.js
+++ simple-express/utils/db.js
@@ -9,6 +9,7 @@ var connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  dateStrings: true,
 });
 //connection->Promise化
 connection = Bluebird.promisifyAll(connection);
