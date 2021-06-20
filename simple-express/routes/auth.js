d388bb20e951c98cd240afc5050f1a103e7389e5
KAO-TZU-WEI <sunvicky11@gmail.com>
2021-06-20 13:39:01 +0800

    add register post function

--- simple-express/routes/auth.js
+++ simple-express/routes/auth.js
@@ -1,10 +1,67 @@
 const express = require("express");
 const router = express.Router();
+const { body, validationResult } = require("express-validator");
+const { connection } = require("../utils/db");
+const bcrypt = require("bcrypt");
+const path = require("path");
+const multer = require("multer");
+
+// const stoarge = multer.diskStorage({
+//   destination: function (req, file, cd) {
+//     //routes/auth.js->現在位置
+//     //public/uploads ->希望找到的位置
+//     //routes/../public/uploads
+//     cb(null, path.join(__dirname, "../", "public", "uploads"));
+//   },
+// });
+// //用一個multer做一個上傳工具
+// const uploader = multer({
+//   storage: myStorage,
+//   fileFilter: function (req, file, cb) {
+//     if (file.mimetype !== "image/jpeg") {
+//       return cb(new Error("不合法的file type"), false);
+//     }
+//     if(!file.orginalname.match(){
+//         return cb(new Error("不合格的副檔名"))
+//     }
+//   },
+// });
 router.get("/register", (req, res) => {
   res.render("auth/register");
 });
-router.post("/register", (req, res) => {
-  res.send("這裡是POST register");
+// 註冊表單資料的驗證規則
+const registerRules = [
+  body("email").isEmail().withMessage("請正確輸入 Email 格式"),
+  body("password").isLength({ min: 4 }),
+  body("confirmPassword").custom((value, { req }) => {
+    return value === req.body.password;
+  }),
+];
+
+router.post("/register", registerRules, async (req, res, next) => {
+  //res.send("這裡是POST register");
+  console.log(req.body);
+  //要做資料驗證
+  const validateResult = validationResult(req);
+  console.log(validateResult);
+  //不是空的就是有問題
+  if (!validateResult.isEmpty()) {
+    return next(new Error("註冊表單資料有問題"));
+  }
+  //先檢查email是否註冊過了
+  let checkResult = await connection.queryAsync(
+    "SELECT * FROM members WHERE email=?",
+    req.body.email
+  );
+  if (checkResult.length > 0) {
+    return next(new Error("已經註冊過了"));
+  }
+  let result = await connection.queryAsync(
+    "INSERT INTO members (email, password, name) VALUES (?)",
+    [[req.body.email, await bcrypt.hash(req.body.password, 10), req.body.name]]
+  );
+
+  res.send("註冊成功");
 });
 router.get("/login", function (req, res) {
   res.render("auth/login");
--- simple-express/server.js
+++ simple-express/server.js
@@ -16,6 +16,17 @@ app.set("view engine", "pug");
 // req -> middlewares..... -> router
 //const data = require("./utils/db");
 
+// 加上這個中間鍵就能解讀POST的資料
+app.use(express.urlencoded({ extended: false }));
+
+let stockRouter = require("./routes/stock");
+let apiRouter = require("./routes/api");
+let authRouter = require("./routes/auth");
+app.use("/stock", stockRouter);
+app.use("/api", apiRouter);
+app.use("/auth", authRouter);
+
+//紀錄來訪中間件
 app.use(function (req, res, next) {
   let current = new Date();
   console.log(`有人來訪了，在${current}`);
@@ -23,12 +34,6 @@ app.use(function (req, res, next) {
   next();
 });
 
-let stockRouter = require("./routes/stock");
-let apiRouter = require("./routes/api");
-app.use("/stock", stockRouter);
-app.use("/api", apiRouter);
-let authRouter = require("./routes/auth");
-app.use("/auth", authRouter);
 //路由router
 //exprerr由上而下執行，找到就停住
 app.get("/", function (req, res) {
@@ -41,8 +46,24 @@ app.get("/test", function (req, res) {
   res.send("test Express");
 });
 
+// 所有的路由的下面
+app.use(function (req, res, next) {
+  // 表示前面的路由都找不到
+  // http status code: 404
+  res.status(404);
+  res.render("404");
+});
+
+// 500 error
+// 放在所有的路由的後面
+// 這裡一定要有4個參數-->最後的錯誤處理
+// express 預設的錯誤處理函式
+app.use(function (err, req, res, next) {
+  console.log("ERROR:", err.message);
+  res.status(500);
+  res.send("500 - Internal Sever Error 請洽系統管理員");
+});
+
 app.listen(3000, () => {
   console.log("我飛起來了，在3000 port");
 });
-
-//網頁出現Cannot GET
